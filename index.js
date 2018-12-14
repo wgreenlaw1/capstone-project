fa09fe21f28a775c48857cc14eb14a70a6da5e4e
William Greenlaw <wgreenlaw1@live.maryville.edu>
2018-12-14 10:17:58 -0800

    Updated

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //jshint esversion: 6
 
-const express = require("expres");
+const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
